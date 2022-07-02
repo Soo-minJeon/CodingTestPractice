d9cb5ef27c95e46b88bc981a9eeb78a08cad4268
smj8554 <smj8554@duksung.ac.kr>
2022-07-02 23:03:15 +0900

    다이나믹 시간 복잡도 (제곱)

--- study_book/ch09/Dijkstra_1.py
+++ study_book/ch09/Dijkstra_1.py
@@ -1,18 +1,41 @@
-# n, m = map(int, input().split())
-# start = int(input())
-#
-# m_list = [[-1 for j in range(n+1)] for i in range(n+1)]
-#
-# # for i in range(m):
-# #     x, y, z = map(int, input().split())
-# #     m_list[x-1][y-1] = z
-# m_list = [[-1, -1, -1, -1, -1, -1, -1], [-1, -1, 2, 5, 1, -1, -1], [-1, -1, -1, 3, 2, -1, -1], [-1, -1, 3, -1, -1, -1, 5], [-1, -1, -1, 3, -1, 1, -1], [-1, -1, -1, 1, -1, -1, 2], [-1, -1, -1, -1, -1, -1, -1]]
-#
-# visited = [0 for i in range(n+1)]
-#
-#
-# def getMin():
-#     tmp_min = -1
-#     for i in range(n+1):
-#         if (tmp_min > )
-#
+n, m = map(int, input().split())
+start = int(input())
+
+m_list = [[-1 for j in range(n+1)] for i in range(n+1)]
+
+# for i in range(m):
+#     x, y, z = map(int, input().split())
+#     m_list[x-1][y-1] = z
+m_list = [[-1, -1, -1, -1, -1, -1, -1], [-1, -1, 2, 5, 1, -1, -1], [-1, -1, -1, 3, 2, -1, -1], [-1, -1, 3, -1, -1, -1, 5], [-1, -1, -1, 3, -1, 1, -1], [-1, -1, -1, 1, -1, -1, 2], [-1, -1, -1, -1, -1, -1, -1]]
+
+visited = [0 for i in range(n+1)]
+answer = [0 for i in range(n+1)]
+for i in range(n):
+    answer[i+1] = m_list[start][i+1]
+
+
+
+
+def getMin():
+    min = int(1e9)
+    index = -1
+
+    for i in range(1, n+1):
+        if (answer[i] < min and visited[i] != 1 and answer[i] > 0):
+            min = answer[i]
+            index = i
+    visited[index] = 1
+    return index
+
+def dynamic():
+    for j in range(n-1):
+        tmp = getMin()
+        for i in range(n+1):
+            if (visited[i]!=1 and m_list[tmp][i]!= -1):
+                if (answer[i] > answer[tmp] + m_list[tmp][i] or answer[i] == -1):
+                    answer[i] = answer[tmp] + m_list[tmp][i]
+        print(tmp, ' : ', answer)
+
+
+dynamic()
+print('\n결과 : ', answer)
--- study_book/test.py
+++ study_book/test.py
@@ -1,10 +1,34 @@
-# 행, 열 입력받기
-N, M = map(int, input().split())
+def solution(grid, k):
+    answer = -1
 
-Input_Array = []
+    dx = [0, 1]
+    dy = [1, 0]
+    move = ['R', 'D']
 
-for i in range(2):
-    Input_Array[i] = list(map(int, input().split()))
+    x, y = 0, 0
+    count = 0
+
+    while(True):
+        for i in range(len(move)):
+            tx = x + dx[i]
+            ty = y + dy[i]
+
+            if (tx>=0 and ty>=0 and tx < len(grid) and ty < len(grid[0]) and grid[tx][ty] != '#'):
+                x,y = tx,ty #이동
+                count += 1
+
+            print(i, ' : ', grid[x][y], '(', x, ', ', y, ')')
+            print(count)
+
+        if (x == len(grid)-1):
+            print('그만')
+            answer = count // k
+            print(answer)
+            break
 
 
-print(Input_Array)
+    return answer
+
+if __name__ == '__main__':
+    grid, k = ["..FF", "###F", "###."], 5
+    solution(grid, k)
